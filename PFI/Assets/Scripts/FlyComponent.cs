5716936be4258c0dc208c7c69cdea3143ae98936
Florence1234567 <119359132+Florence1234567@users.noreply.github.com>
2023-05-13 11:56:25 -0400

    a

--- PFI/Assets/Scripts/FlyComponent.cs
+++ PFI/Assets/Scripts/FlyComponent.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class FlyComponent : MonoBehaviour
 {
+    Coroutine LaunchCoroutine;
+
+    Vector3 landingPos;
+
+    float speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,23 @@ public class FlyComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Calcul de la parabole du mouvement du personnage.
+        
+    }
+
+    public void Launch(Vector3 landingPos)
+    { 
+        this.landingPos = landingPos;
+        LaunchCoroutine = StartCoroutine("Launch");
+    }
+
+    IEnumerator Launch()
+    {
+        //Calculate parabola.
+
+        for(int i = 0; i < 10; i++)
+        {
+            //Move character.
+            yield return new WaitForSeconds(1f);
+        }
     }
 }
